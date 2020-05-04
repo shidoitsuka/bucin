351906ced053ce9dbdfaf6c796c7561569bd474f
NakaChan <42824788+NakaChans@users.noreply.github.com>
2020-05-05 03:23:07 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,9 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "@lianovitaa", quotes: "Cukup jaringan aja yang hilang, kamu jangan." },
+    { author: "@lianovitaa", quotes: "Berjanjilah untuk terus bersamaku sekarang, esok dan selamanya." },
+    { author: "@lianovitaa", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "@lianovitaa", quotes: "I love you." },
+    { author: "@lianovitaa", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "@lianovitaa", quotes: "I love you." },
 ];
