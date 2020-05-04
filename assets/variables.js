6c0981be7644ff789e9c3917b72281f7e5b2fdd1
Brian <mail@standinshd.site>
2020-05-04 21:10:01 +0700

    removed example files

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
