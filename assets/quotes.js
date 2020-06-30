546f8eed826fcdce73d13d9cf5d2d999ff6aa238
fkrarf03 <67625560+fkrarf03@users.noreply.github.com>
2020-06-30 08:10:33 +0000

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
+    { author: "avrill.v", quotes: "kamu cewek yg paling kusayang<3" },
+    { author: "antjedtta_", quotes: "huhuuuu:)" },
+    { author: "standinshd", quotes: "Heiii! I LOVE U SO MUCH<3" },
+    { author: "standinshd", quotes: "I love youuuuuuuuuuu" },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "standinshd", quotes: "Imiss u so muchhh" },
+    { author: "standinshd", quotes: "Intinya aku sayang samamu" },
 ];
