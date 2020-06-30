82051c3633272d09726432d09a7d76c3348731e7
fkrarf03 <67625560+fkrarf03@users.noreply.github.com>
2020-06-30 15:30:13 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["bby", "Babe", "Sayanggg", "beybbbb", bebyyy"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
